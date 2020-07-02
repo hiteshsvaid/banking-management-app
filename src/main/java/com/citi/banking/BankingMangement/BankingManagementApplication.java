886e26d718b7f95c9e7d066f1f734d0391adaf32
Hitesh Vaid <hiteshsvaid@gmail.com>
2020-07-01 19:19:27 -0500

    Adding spring intializer generated project

---
+++ src/main/java/com/citi/banking/BankingMangement/BankingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.citi.banking.BankingMangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citi/banking/BankingMangement/BankingMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.banking.BankingMangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
