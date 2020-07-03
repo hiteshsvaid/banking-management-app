95b2f2f1d0795956cc6771586654ddf9e2569592
Hitesh Vaid <hiteshsvaid@gmail.com>
2020-07-03 12:26:22 -0500

    Transfer Fund view wired with rest controller.

--- src/main/java/com/citi/banking/BankingManagement/dto/AccountTransferResponse.java
+++ src/main/java/com/citi/banking/BankingManagement/dto/AccountTransferResponse.java
@@ -1,4 +1,10 @@
 package com.citi.banking.BankingManagement.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class AccountTransferResponse {
+    private boolean errorExist;
 }
